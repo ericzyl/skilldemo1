e7808bb00045df9b74b3f7df655c715255af11b7
ericzyl <87045758+ericzyl@users.noreply.github.com>
2022-02-02 14:09:27 -0800

    update 1

---
+++ SkillDemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    @Test
+    public void testAdd(){
+        assertEquals(2, SkillDemo.add(1, 2));
+    }
+}
